7496874cdb746a26adecd04c20a30ec68848d9be
Cristian Ferreira <ferreira.marques.box@gmail.com>
2023-07-14 11:09:53 -0300

    voltando para verção linkando

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("hub do site da tchêstorm")
-console.log("aplicação está se coectando ao banco de dados")
-//mais um console log de teste
-console.log("O Paulo fez esse commit sem voce ver na aula")
