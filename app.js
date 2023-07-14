1a244a3ae2fa82e9acb8996e2523bd2bddf69e01
Cristian Ferreira <ferreira.marques.box@gmail.com>
2023-07-14 10:49:14 -0300

    add mais console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("hub do site da tchêstorm")
+console.log("aplicação está se coectando ao banco de dados")
+//mais um console log de teste
+console.log("O Paulo fez esse commit sem voce ver na aula")
